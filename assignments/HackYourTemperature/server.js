414c2ed1b4ab54266e193a1ee99bb43bffb6c1fb
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-05 01:09:55 +0200

    5.1 Adding a POST request step is done

--- assignments/HackYourTemperature/server.js
+++ assignments/HackYourTemperature/server.js
@@ -2,7 +2,15 @@ import express from "express";
 const server = express();
 const PORT = 3000;
 
+// Add middleware to parse JSON data
+server.use(express.json());
+
 server.get("/", (req, res) => res.send("hello from backend to frontend!"));
+
+server.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
 
 server.listen(PORT, () =>
   console.log(`Server is listening on http://localhost:${PORT}`)
