7bfdd58617d69254bf978e26e7b602cf222b260c
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-04 13:46:33 +0200

    2-left-pad works

--- week1/practice-exercises/2-left-pad/script.js
+++ week1/practice-exercises/2-left-pad/script.js
@@ -1,13 +1,17 @@
 /**
  ** Exercise 2: To the left, to the left...
- * 
+ *
  * Copy and paste your code from the previous exercise.
- * Replace the function `padLeft` to use
+ * Replace the function `leftPad` to use
  * this new NPM package called `left-pad` instead then
  * Pad the numbers to 8 characters to confirm that it works correctly
  *
  */
 
-let numbers = [ "12", "846", "2", "1236" ];
+let numbers = ["12", "846", "2", "1236"];
 
-// YOUR CODE GOES HERE
+const leftPad = require("left-pad");
+
+numbers.forEach((num) => {
+  console.log(leftPad(num, 8, "_"));
+});
