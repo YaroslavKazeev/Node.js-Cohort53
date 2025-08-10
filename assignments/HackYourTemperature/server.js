99188b15cb5e8931abf0dddae42514042e97a25f
Yaroslav <1.yaroslav.kazeev@gmail.com>
2025-08-10 19:30:14 +0200

    the previos hackyourtemperature steps commited

---
+++ assignments/HackYourTemperature/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+const server = express();
+const PORT = 3000;
+
+// Add middleware to parse JSON data
+server.use(express.json());
+
+server.get("/", (req, res) => res.send("hello from backend to frontend!"));
+
+server.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.send(cityName);
+});
+// test text for making the request on Postman
+// {
+//   "cityName": "Amsterdam"
+// }
+
+server.listen(PORT, () =>
+  console.log(`Server is listening on http://localhost:${PORT}`)
+);
